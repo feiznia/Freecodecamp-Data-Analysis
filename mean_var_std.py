12dcf3106a11ab96db908967c4bd5c6d5970ccf1
Diba Feiznia <72469950+feiznia@users.noreply.github.com>
2024-08-07 18:11:09 +0330

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -5,13 +5,13 @@ def calculate(lst):
     if len(lst) < 9:
         raise ValueError("List must contain nine numbers.")
     else:
-        mat = [lst[0:3],lst[3:6],lst[6:]]
+        mat = np.vstack([lst[0:3],lst[3:6],lst[6:]])
         calculations = {
-          'mean': [np.mean(mat, axis = 0), np.mean(mat, axis = 1),np.mean(lst)],
-          'variance': [np.var(mat, axis = 0), np.var(mat, axis = 1), np.var(lst)],
-          'standard deviation': [np.std(mat, axis = 0), np.std(mat, axis = 1), np.std(lst)],
-          'max': [np.max(mat, axis = 0), np.max(mat, axis = 1), np.max(lst)],
-          'min': [np.min(mat, axis = 0), np.min(mat, axis = 1), np.min(lst)],
-          'sum': [np.sum(mat, axis = 0), np.sum(mat, axis = 1), np.sum(lst)]
+          'mean': [np.mean(mat, axis = 0).tolist(), np.mean(mat, axis = 1).tolist(), np.mean(lst)],
+          'variance': [np.var(mat, axis = 0).tolist(), np.var(mat, axis = 1).tolist(), np.var(lst)],
+          'standard deviation': [np.std(mat, axis = 0).tolist(), np.std(mat, axis = 1).tolist(), np.std(lst)],
+          'max': [np.max(mat, axis = 0).tolist(), np.max(mat, axis = 1).tolist(), np.max(lst)],
+          'min': [np.min(mat, axis = 0).tolist(), np.min(mat, axis = 1).tolist(), np.min(lst)],
+          'sum': [np.sum(mat, axis = 0).tolist(), np.sum(mat, axis = 1).tolist(), np.sum(lst)]
         }
         return calculations
